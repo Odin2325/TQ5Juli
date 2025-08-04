2d97bddea4a4c933edb835ede6d493d093fce127
Odin2325 <nico.arevalo2325@gmail.com>
2025-08-04 12:01:30 +0200

    Sturkturierung

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+username = input("Bitte geben Sie Ihren Benutzernamen ein: ")
+passwort = input("Bitte geben Sie Ihr Passwort ein: ")
+
+if username == "user123" and passwort == "123456789":
+    print("Login erfolgreich!")
+else:
+    print("Login fehlgeschlagen!")
+
+
+if username == "user123":
+    #weiters code
+    if passwort == "123456789":
+        print("Login erfolgreich!")
+else:
+    print("Login fehlgeschlagen!")
