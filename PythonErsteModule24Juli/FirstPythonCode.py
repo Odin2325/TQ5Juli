e00122574a1f4d00b80dfe5376d601ebf925a598
Ron1977 <ron.rossbach77@gmail.com>
2025-08-13 08:58:49 +0200

    Übung

--- PythonErsteModule24Juli/FirstPythonCode.py
+++ PythonErsteModule24Juli/FirstPythonCode.py
@@ -1,1 +1,9 @@
 print('Hallo World')
+
+
+mein_string = 'Dies ist ein Beispieltext.'
+mein_anderer_string = 'Dies ist ein anderer Beispieltext.'
+print(mein_string,end='\n')
+print('''Dies ist ein Beispieltext.
+
+Dies ist ein anderer Beispieltext.''')
