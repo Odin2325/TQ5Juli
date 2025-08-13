dc5defdf45d7f73888715c6c4f07bf3d0318d194
Ron1977 <ron.rossbach77@gmail.com>
2025-08-13 12:33:18 +0200

    Lösung13August

---
+++ Loesung13August.py
@@ -0,0 +1,11 @@
+# Erstellen Sie zunächst drei Variablen: Ihr Lieblingsauto, Ihr Lieblingssport und Ihr Lieblingsgetränk.
+
+
+# Speichern Sie dies nun als formatierte Zeichenfolge (f-string) in einer Variablen, so dass es wie folgt aussieht:
+# 'Wenn nur mein [mein_Lieblingsauto] [mein_Lieblingsgetränk] als Kraftstoff verwenden könnte, um [mein_Lieblingssport] auszuüben. Dann wäre ich zufrieden.'
+
+
+# 1. Normale Variante ausgeben.
+
+
+# 2. Geben Sie nun diese Zeichenfolge unter Verwendung von eckigen Klammern vom 6. bis 16. inklusive Index aus.
