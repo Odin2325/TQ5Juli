b635b66225cf3c95fe38279f9f2dffb4c85b8951
Ron1977 <ron.rossbach77@gmail.com>
2025-08-18 09:01:37 +0200

    Changes

---
+++ Uebung_2025-08-14.py
@@ -0,0 +1,6 @@
+print('Berechnen wir die Fläche eines Dreiecks.')
+print('Bitte geben Sie nur ganze Zahlen ein.')
+breite = int(input('Geben Sie die Basis des Dreiecks ein:'))
+hoehe = int(input('Geben Sie die Höhe des Dreiecks ein:'))
+flaeche = (breite * hoehe) / 2
+print(f'Die Fläche des Dreiecks beträgt: {flaeche} Quadrat-Einheiten.')
