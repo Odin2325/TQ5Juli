1618a625a845379348c4fc59fe406580dae70b9b
Hananekt <h.neaimate@gmail.com>
2025-08-19 14:41:31 +0200

    zuspät

---
+++ AufgabenUndLoesungen/verschiden/Aufgabe_130825.py
@@ -0,0 +1,16 @@
+# mein_Lieblingsauto = [input (str("geben sie ihr Lieblingsauto ein : "))]
+# mein_Lieblingsgetränk = [input (str("geben sie ihr Lieblingsgetränk ein : "))]
+# meinen_Lieblingssport = [input (str("geben sie ihren Lieblingssport ein : "))]
+
+
+mein_Lieblingsauto = " Mercedes "
+mein_Lieblingsgetränk = " Kaffee "
+meinen_Lieblingssport = " Reiten "
+
+
+
+# 1
+print ("Wenn nur "+mein_Lieblingsauto+ mein_Lieblingsgetränk+ "als Kraftstoff verwenden könnte, um "+ meinen_Lieblingssport+ "auszuüben.\nDann wäre ich zufrieden!")
+
+# 2
+print ("Wenn nur "+ mein_Lieblingsauto[6:16]+ mein_Lieblingsgetränk[6:17]+ "als Kraftstoff verwenden könnte, um "+ meinen_Lieblingssport[6:16]+ "auszuüben.\nDann wäre ich zufrieden!")
