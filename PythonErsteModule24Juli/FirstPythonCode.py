f905290805f2e0372aa7ef176bc7f8aafdb65a61
Ron1977 <ron.rossbach77@gmail.com>
2025-08-20 19:27:24 +0200

    Hoffentlich habe ich nur die Snapshots in meiner Branch gelöscht.

--- PythonErsteModule24Juli/FirstPythonCode.py
+++
@@ -1,9 +0,0 @@
-print('Hallo World')
-
-
-mein_string = 'Dies ist ein Beispieltext.'
-mein_anderer_string = 'Dies ist ein anderer Beispieltext.'
-print(mein_string,end='\n')
-print('''Dies ist ein Beispieltext.
-
-Dies ist ein anderer Beispieltext.''')
--- PythonErsteModule24Juli/ModuleBeispiel.py
+++
@@ -1,5 +0,0 @@
-def summe(liste):
-    sum = 0
-    for zahl in liste:
-        sum += zahl
-    return sum
