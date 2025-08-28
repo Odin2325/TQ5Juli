a360779627d917a70de23bde7384a8aed0a82e67
Odin2325 <nico.arevalo2325@gmail.com>
2025-08-28 10:20:24 +0200

    BankkontoVerzeichnis

---
+++ AufgabenUndLoesungen/Python/Banksystem/Bankkonto.py
@@ -0,0 +1,56 @@
+from random import randint
+from datetime import datetime
+
+class Bankkonto:
+    def __init__(self, kontoinhaber, adresse, startguthaben=0):
+        self.kontoinhaber = kontoinhaber
+        self.adresse = adresse
+        self.kontostand = startguthaben
+        self.bank = 'Deutsche Bank'
+        self.region = 'Bayern'
+        self.kontonummer = 'DE12346789012'
+        self.pin = Bankkonto.pin_generator()
+        self.transaktions_historie = []
+
+    def pin_generator():
+        neues_pin = ''
+        for i in range(0,4):
+            neues_pin += str(randint(0,9))
+        print('Das neue Pin ist:',neues_pin)
+        return neues_pin
+
+    def details(self):
+        print('Konto Details')
+        print('=======================================')
+        print(f'Kontoinhaber: {self.kontoinhaber}\nAdresse: {self.adresse}')
+        print(f'Bank: {self.bank}\nRegion: {self.region}')
+        print(f'Kontonummer: {self.kontonummer}\nKontostand: {self.kontostand}')
+        print('=======================================')
+
+    def einzahlen(self, betrag):
+        if betrag <= 0:
+            print('0 und Negative Betraege sind nicht erlaubt.')
+            return False
+        self.kontostand += betrag
+        self.transaktions_historie.append(('einzahlen',betrag,self.kontostand,datetime.now()))
+        print('Einzahlung erfolgreich.')
+        return True
+        
+    def auszahlen(self, betrag):
+        if betrag <= 0:
+            print('0 und Negative Betraege sind nicht erlaubt.')
+            return False
+        self.kontostand -= betrag
+        self.transaktions_historie.append(('auszahlen',betrag,self.kontostand,datetime.now()))
+        print('Auszahlung erfolgreich')
+        return True
+    
+    def kontostand_ansehen(self):
+        print('Dein Aktueller Kontostand betraegt:',self.kontostand)
+
+    def kontoauszug(self):
+        print('==========Transaktionshistorie==========')
+        print('(operation,betrag,kontostand,timestamp)')
+        for eintrag in self.transaktions_historie:
+            print(eintrag)
+        print('==========EndeHistorie==========')
