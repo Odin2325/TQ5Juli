c42df085ed75cd09ca9e524c9a92906acb9d820f
sabasarah2007 <sabafarhan@outlook.de>
2025-08-28 12:57:05 +0200

    sabakunde1

---
+++ PythonErsteModule24Juli/kunde.py
@@ -0,0 +1,19 @@
+class Kunde:
+    def __init__(self, vorname, nachname, adresse, telefonnummer, email, alter):
+        self.vorname = vorname
+        self.nachname = nachname
+        self.adresse = adresse
+        self.telefonnumer = telefonnummer
+        self.email = email
+        self.alter = alter
+    
+    def detail_kunde(self,formatier_anzeige):
+        if formatier_anzeige:
+            print('===========KundeDetails===========')
+            print(f'Name: {self.vorname} {self.nachname}')
+            print(f'Adresse: {self.adresse}. Email: {self.email}')
+            print(f'Telefonnummer: {self.telefonnumer}')
+            print('Alter:',self.alter)
+            print('==================================')
+        else:
+            return f'{self.vorname} {self.nachname} {self.email}'
