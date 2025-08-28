d17fdc352f96fee09e7d790a216c952d457b5896
sabasarah2007 <sabafarhan@outlook.de>
2025-08-28 12:53:16 +0200

    sababankkonto1

--- PythonErsteModule24Juli/bankkonto.py
+++ PythonErsteModule24Juli/bankkonto.py
@@ -1,14 +1,56 @@
+from random import randint
+from datetime import datetime
+
 class Bankkonto:
-    def __init__(self,kontoinhaber,kontonummer,startguthaben=0):
+    def __init__(self, kontoinhaber:Kunde, startguthaben=0):
         self.kontoinhaber = kontoinhaber
-        self.kontonummer = kontonummer
-        self.startguthaben = startguthaben
-        self.Währung ='EUR'
-        self.bankname ='sparkasse'
+        self.kontostand = startguthaben
+        self.bank = 'Deutsche Bank'
+        self.region = 'Bayern'
+        self.kontonummer = 'DE12346789012'
+        self.pin = Bankkonto.pin_generator()
+        self.transaktions_historie = []
 
-    def bank_detail(self):
-        print(f"bankdetail:\n kontoinhab={self.kontoinhaber}\n kontonummer = {self.kontonummer}\n startguthaben ={self.startguthaben}\n wärung = {self.Währung}\n bankname={self.bankname}")   
+    def pin_generator():
+        neues_pin = ''
+        for i in range(0,4):
+            neues_pin += str(randint(0,9))
+        print('Das neue Pin ist:',neues_pin)
+        return neues_pin
 
+    def details(self):
+        print('Konto Details')
+        print('=======================================')
+        print(f'Kontoinhaber: {self.kontoinhaber.detail_kunde(False)}')
+        print(f'Bank: {self.bank}\nRegion: {self.region}')
+        print(f'Kontonummer: {self.kontonummer}\nKontostand: {self.kontostand}')
+        print('=======================================')
 
-mein_konto = Bankkonto('saba',34527799,53) 
-mein_konto.bank_detail()
+    def einzahlen(self, betrag):
+        if betrag <= 0:
+            print('0 und Negative Betraege sind nicht erlaubt.')
+            return False
+        self.kontostand += betrag
+        self.transaktions_historie.append(('einzahlen',betrag,self.kontostand,datetime.now()))
+        print('Einzahlung erfolgreich.')
+        return True
+        
+    def auszahlen(self, betrag):
+        if betrag <= 0:
+            print('0 und Negative Betraege sind nicht erlaubt.')
+            return False
+        self.kontostand -= betrag
+        self.transaktions_historie.append(('auszahlen',betrag,self.kontostand,datetime.now()))
+        print('Auszahlung erfolgreich')
+        return True
+    
+    def kontostand_ansehen(self):
+        print('Dein Aktueller Kontostand betraegt:',self.kontostand)
+
+    def kontoauszug(self):
+        print('==========Transaktionshistorie==========')
+        print('(operation,betrag,kontostand,timestamp)')
+        for eintrag in self.transaktions_historie:
+            print(eintrag)
+        print('==========EndeHistorie==========')
+        
