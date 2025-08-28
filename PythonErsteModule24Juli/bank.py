c16a91313164aba1fa2ddcd492970afe01d6ee96
sabasarah2007 <sabafarhan@outlook.de>
2025-08-28 14:02:30 +0200

    bankänderung

---
+++ PythonErsteModule24Juli/bank.py
@@ -0,0 +1,11 @@
+class bank:
+    def __init__(self,name,region):
+        self.name = name
+        self.region = region
+
+    def  banksuchen_deteil(self,alle_anzeige):
+        if  alle_anzeige:
+            print({f"{self.name},{self.region}"})
+        else:
+            return f'{self.name}, {self.region}' 
+        
