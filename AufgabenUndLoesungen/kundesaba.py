00348220f1408f87b52d9e6b92ada981b1be6ba9
sabasarah2007 <sabafarhan@outlook.de>
2025-09-13 22:54:33 +0200

    saba

---
+++ AufgabenUndLoesungen/kundesaba.py
@@ -0,0 +1,14 @@
+class Kunde:
+    def __init__(self, vorname,nachname, adresse,telefonnummer):
+        self.vorname=vorname
+        self.nachname=nachname
+        self.adresse=adresse
+        self.telefonnummer=telefonnummer
+        bakkontos=[]
+
+    def details(self,zeige_alles):
+       if zeige_alles:
+        text= print(f"vorname:{self.vorname}\n nachnam:{self.nachname}\n adress:{self.adresse}\n telefonnummer:{self.telefonnummer}")
+        return text
+       else:
+          return(f"voename:{self.vorname}\n nachname:{self.nachname}")
